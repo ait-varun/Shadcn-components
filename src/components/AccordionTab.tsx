562593a091c75477c8fdcbdae909219f651aa711
varun <aitvarunbhatia@gmail.com>
2024-04-09 17:18:16 +0530

    SHADCN STARTER

---
+++ src/components/AccordionTab.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+export default function AccordionTab() {
+  return (
+    <>
+      <Accordion type="multiple" className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </>
+  );
+}
