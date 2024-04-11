7e829c594dd8e91f27bee23b0168a6d78715fa37
varun <aitvarunbhatia@gmail.com>
2024-04-11 11:16:14 +0530

    updated design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -55,52 +55,54 @@ export default function Header() {
 
   return (
     <>
-      <div className="flex items-center justify-between px-4 py-2 bg-blue-400 text-white">
+      <div className=" px-4 w-full flex py-2 bg-blue-400 text-white ">
         {/* Logo */}
-        <div className="text-xl font-bold">Logo</div>
+        <div className="text-xl font-bold w-64">Logo</div>
 
-        {/* Search Input */}
-        <div className="relative">
-          <svg
-            className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        <div className="flex grow justify-between">
+          {/* Search Input */}
+          <div className="relative">
+            <svg
+              className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchInput}
+              onChange={handleChange}
+              className="px-10 py-2 rounded-full bg-white text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-          </svg>
-          <input
-            type="text"
-            placeholder="Search..."
-            value={searchInput}
-            onChange={handleChange}
-            className="px-10 py-2 rounded-full bg-white text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          {filteredCountries.length > 0 && (
-            <ul className="absolute z-10 w-full bg-white text-black shadow-lg rounded-md mt-2">
-              {filteredCountries.map((country, index) => (
-                <li
-                  key={index}
-                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                  {country.name}
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-
-        {/* Avatar */}
-        <div className="flex items-center">
-          <BellIcon className="h-5 w-5 mr-8" />
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+            {filteredCountries.length > 0 && (
+              <ul className="absolute z-10 w-full bg-white text-black shadow-lg rounded-md mt-2">
+                {filteredCountries.map((country, index) => (
+                  <li
+                    key={index}
+                    className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                    onClick={() => handleCountryClick(country.name)}>
+                    {country.name}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+          {/* Avatar */}
+          <div className="flex items-center">
+            <BellIcon className="h-5 w-5 mr-8" />
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
         </div>
       </div>
     </>
