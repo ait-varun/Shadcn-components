b90c85048565c20f919921b37053ae490046ef2d
varun <aitvarunbhatia@gmail.com>
2024-04-11 15:05:52 +0530

    updated

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,10 +1,12 @@
 import Products from "@/components/Products";
+import { SheetDemo } from "@/components/SheetDemo";
 
 export default function Contact() {
   return (
     <>
-      <main className="md:ml-40 z-10">
+      <main className="md:ml-40">
         <Products />
+        <SheetDemo />
       </main>
     </>
   );
---
+++ src/components/SheetDemo.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+export function SheetDemo() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">Open</Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Add New Task</SheetTitle>
+          {/* <SheetDescription>
+            Make changes to your profile here. Click save when you're done.
+          </SheetDescription> */}
+        </SheetHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-rows-2 items-center">
+            <Label htmlFor="name">Task Name</Label>
+            <Input id="name" value="Enter Task Name" className="col-span-3" />
+          </div>
+          <div className="grid grid-rows-2 items-center">
+            <Label htmlFor="username">Assigned To</Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button type="submit">Save changes</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+}
