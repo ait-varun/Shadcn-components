b176d7fc1465282da7ecb40728dce5f8ad9bed2f
varun <aitvarunbhatia@gmail.com>
2024-04-11 14:58:05 +0530

    updated

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,6 +1,4 @@
-import Header from "@/components/Header";
 import Products from "@/components/Products";
-import SidebarMenu from "@/components/Sidebar-menu";
 
 export default function Contact() {
   return (
