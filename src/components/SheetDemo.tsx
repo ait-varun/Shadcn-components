5851237d87cc39bffaa9d702f72fb277143801bb
varun <aitvarunbhatia@gmail.com>
2024-04-11 15:34:06 +0530

    fixed bugs

--- src/components/SheetDemo.tsx
+++ src/components/SheetDemo.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+
 import {
   Sheet,
   SheetClose,
@@ -11,6 +14,15 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export function SheetDemo() {
   return (
@@ -28,11 +40,28 @@ export function SheetDemo() {
         <div className="grid gap-4 py-4">
           <div className="grid grid-rows-2 items-center">
             <Label htmlFor="name">Task Name</Label>
-            <Input id="name" value="Enter Task Name" className="col-span-3" />
+            <Input
+              id="name"
+              defaultValue="Enter Task Name"
+              className="col-span-3"
+            />
           </div>
           <div className="grid grid-rows-2 items-center">
             <Label htmlFor="username">Assigned To</Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
+            <Select>
+              <SelectTrigger>
+                <SelectValue placeholder="Select Team Member" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="1">Alex</SelectItem>
+                  <SelectItem value="2">Sam</SelectItem>
+                  <SelectItem value="3">Jimmy</SelectItem>
+                  <SelectItem value="4">John</SelectItem>
+                  <SelectItem value="5">Anny</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </div>
         </div>
         <SheetFooter>
