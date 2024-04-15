2251015947f7d8bcbe63550629bc3a4f1f65d4a5
varun <aitvarunbhatia@gmail.com>
2024-04-15 17:35:48 +0530

    added scroll bar

--- src/components/TabDemo.tsx
+++ src/components/TabDemo.tsx
@@ -3,15 +3,54 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { CardsDataTable } from "./data-table";
 import Products from "./Products";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 
 export default function TabDemo() {
   return (
     <>
-      <Tabs defaultValue="payment" className="w-full">
-        <TabsList>
-          <TabsTrigger value="payment">Payment</TabsTrigger>
-          <TabsTrigger value="password">Products</TabsTrigger>
-        </TabsList>
+      <Tabs defaultValue="payment" className="">
+        <ScrollArea className="w-full">
+          <TabsList>
+            <TabsTrigger value="payment" className="w-36">
+              Payment
+            </TabsTrigger>
+            <TabsTrigger value="password" className="w-36">
+              {" "}
+              Products
+            </TabsTrigger>
+            <TabsTrigger value="Support" className="w-36">
+              Support
+            </TabsTrigger>
+            <TabsTrigger value="About" className="w-36">
+              About
+            </TabsTrigger>
+            <TabsTrigger value="Contact" className="w-36">
+              Contact
+            </TabsTrigger>
+            <TabsTrigger value="Suppliers" className="w-36">
+              Suppliers
+            </TabsTrigger>
+            <TabsTrigger value="Items" className="w-36">
+              Items
+            </TabsTrigger>
+            <TabsTrigger value="Categories" className="w-36">
+              Categories
+            </TabsTrigger>
+            <TabsTrigger value="Transactions" className="w-36">
+              Transactions
+            </TabsTrigger>
+            <TabsTrigger value="Sales" className="w-36">
+              Sales
+            </TabsTrigger>
+            <TabsTrigger value="Purchases" className="w-36">
+              Purchases
+            </TabsTrigger>
+            <TabsTrigger value="Units" className="w-36">
+              Units
+            </TabsTrigger>
+          </TabsList>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
         <TabsContent value="payment">
           <CardsDataTable />
         </TabsContent>
