0e7a50c829f2208c40dc19619a078b59bab291c5
varun <aitvarunbhatia@gmail.com>
2024-04-15 16:53:48 +0530

    updated

--- src/components/TabDemo.tsx
+++ src/components/TabDemo.tsx
@@ -7,7 +7,7 @@ import Products from "./Products";
 export default function TabDemo() {
   return (
     <>
-      <Tabs defaultValue="payment" className="">
+      <Tabs defaultValue="payment" className="w-full">
         <TabsList>
           <TabsTrigger value="payment">Payment</TabsTrigger>
           <TabsTrigger value="password">Products</TabsTrigger>
